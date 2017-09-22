023ed792034e9251c88acad358012fcbbb17bd74
Nurbek <nurbek@inbox.ru>
2017-09-22 15:14:19 +0400

    release

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Frequency Analysis of Words
+The script shows 10 most frequent words in a file
 
-[TODO. There will be project description]
+# Quickstart
+Launch on Linux( Python 3.5 ) or Windows as simple
+```#!bash
+$ python lang_frequency.py <path_to_file>
+# For example, python lang_frequency.py '/home/user/downloads/file.txt'
+```
 
 # Project Goals
-
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,46 @@
+import os
+
+
+frequent_words_list = ['и', 'в', 'не', 'на', 'я', 'быть', 'он', 'с', 'что',
+                       'по', 'это', 'она', 'этот', 'к', 'но', 'они', 'мы',
+                       'как', 'из', 'у', 'который', 'то', 'за', 'свой', 'что',
+                       'весь', 'год', 'от', 'так', 'о', 'для', 'ты', 'же',
+                       'все', 'тот', 'мочь', 'вы', 'человек', 'такой', 'его',
+                       'сказать', 'только', 'или', 'ещё', 'бы', 'себя', 'один',
+                       'как', 'уже', 'до', 'время', 'если', 'сам', 'когда',
+                       'другой', 'вот', 'говорить', 'наш', 'мой', 'знать',
+                       'стать', 'при', 'чтобы', 'дело', 'жизнь', 'кто',
+                       'первый', 'очень', 'два', 'день', 'её', 'новый', 'рука',
+                       'даже', 'во', 'со', 'раз', 'где', 'там', 'под', 'можно',
+                       'ну', 'какой', 'после', 'их', 'работа', 'без', 'самый',
+                       'потом', 'надо', 'хотеть', 'ли', 'слово', 'идти',
+                       'большой', 'должен', 'место', 'иметь', 'ничто',  'а'
+                       ]
+
+result_dict = {i: 0 for i in frequent_words_list}
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath) as source_file:
+	    for line in source_file:
+            for i in line.rstrip().split(' '):
+                for j in range(0, len(frequent_words_list)):
+                    if frequent_words_list[j] == i:
+                        result_dict[frequent_words_list[j]] += 1
+    return result_dict
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(words_pool):
+    for i in sorted(words_pool.items(), key=lambda x: x[1], reverse=True)[:10]:
+        print('Слово ' + '\'' + i[0] + '\'',
+              ' встречается ' + str(i[1]) + 'раз')
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+        get_most_frequent_words(load_data(filepath))
+    else:
+        print("Error! Enter path!")
