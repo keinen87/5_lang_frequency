9fab6a9a41b6d0c0ee316056bfd5412b54bcc398
Nurbek <nurbek@inbox.ru>
2017-09-25 09:39:27 +0400

    Fixed algorythm. Used build-in function.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,42 +1,22 @@
+import collections
 import os
 import sys
 
 
-frequent_words_list = ['и', 'в', 'не', 'на', 'я', 'быть', 'он', 'с', 'что',
-                       'по', 'это', 'она', 'этот', 'к', 'но', 'они', 'мы',
-                       'как', 'из', 'у', 'который', 'то', 'за', 'свой', 'что',
-                       'весь', 'год', 'от', 'так', 'о', 'для', 'ты', 'же',
-                       'все', 'тот', 'мочь', 'вы', 'человек', 'такой', 'его',
-                       'сказать', 'только', 'или', 'ещё', 'бы', 'себя', 'один',
-                       'как', 'уже', 'до', 'время', 'если', 'сам', 'когда',
-                       'другой', 'вот', 'говорить', 'наш', 'мой', 'знать',
-                       'стать', 'при', 'чтобы', 'дело', 'жизнь', 'кто',
-                       'первый', 'очень', 'два', 'день', 'её', 'новый', 'рука',
-                       'даже', 'во', 'со', 'раз', 'где', 'там', 'под', 'можно',
-                       'ну', 'какой', 'после', 'их', 'работа', 'без', 'самый',
-                       'потом', 'надо', 'хотеть', 'ли', 'слово', 'идти',
-                       'большой', 'должен', 'место', 'иметь', 'ничто',  'а'
-                       ]
-
-result_dict = {frequent_word: 0 for frequent_word in frequent_words_list}
-
-
 def load_data(filepath):
+    collections_counter = collections.Counter()
     if not os.path.exists(filepath):
         return None
     with open(filepath) as source_file:
         for line in source_file:
             for word in (line.rstrip()).split(' '):
-                for frequent_word in range(len(frequent_words_list)):
-                    if frequent_words_list[frequent_word] == word:
-                        result_dict[frequent_words_list[frequent_word]] += 1
-    return result_dict
-
+                collections_counter[word] +=1
+    return collections_counter
 
 def get_most_frequent_words(words_dict):
-    for pair in sorted(words_dict.items(), key=lambda x: x[1], reverse=True)[:10]:
+    for pair in words_dict.most_common(10):
         print('Слово ' + '\'' + pair[0] + '\'',
-              ' встречается ' + str(pair[1]) + 'раз')
+              ' встречается ' + str(pair[1]) + ' раз(а)')
 
 
 if __name__ == '__main__':
