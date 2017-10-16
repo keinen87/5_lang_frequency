9193263599e928b9ccbe2774dce392437085996b
Nurbek <nurbek@inbox.ru>
2017-10-16 21:54:24 +0400

    Fixed readme, split logic on functions, error using Counter and print output format

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 The script shows 10 most frequent words in a file
 
 # Quickstart
+You may put in script folder a txt file(Where_are_you.txt in repository - example) or input path to file
 Launch on Linux( Python 3.5 ) or Windows as simple
-```#!bash
-$ python lang_frequency.py <path_to_file>
-# For example, python lang_frequency.py '/home/user/downloads/file.txt'
+```bash
+# For example,
+$ python lang_frequency.py Where_are_you.txt
+# You will see:
+ The word 'и' meet 1427 time(s)
+ The word 'в' meet 1343 time(s)
+ The word 'на' meet 951 time(s)
+ The word 'не' meet 950 time(s)
+ The word 'что' meet 650 time(s)
+ The word 'с' meet 534 time(s)
+ The word 'я' meet 460 time(s)
+ The word 'она' meet 420 time(s)
+ The word 'к' meet 365 time(s)
+ The word 'ты' meet 356 time(s)
 ```
 
 # Project Goals
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,28 +1,29 @@
+import argparse
 import collections
-import os
 import sys
 
 
 def load_data(filepath):
-    collections_counter = collections.Counter()
-    if not os.path.exists(filepath):
-        return None
     with open(filepath) as source_file:
-        for line in source_file:
-            for word in (line.rstrip()).split(' '):
-                collections_counter[word] +=1
-    return collections_counter
+        raw_data = [(line.strip()).split(' ') for line in source_file]
+    return raw_data
 
-def get_most_frequent_words(words_dict):
+
+def data_prepare(raw_data):
+    words_list = [item for element in raw_data for item in element ]
+    return words_list
+
+
+def get_most_frequent_words(words_list):
     most_popular_words = 10
-    for pair in words_dict.most_common(most_popular_words):
-        print('Слово ' + '\'' + pair[0] + '\'',
-              ' встречается ' + str(pair[1]) + ' раз(а)')
+    for pair in collections.Counter(words_list).most_common(most_popular_words):
+        #print(pair[0], pair[1])
+        print('The word \'{}\' meets {} time(s)'.format(pair[0],pair[1]))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        filepath = sys.argv[1]
-        get_most_frequent_words(load_data(filepath))
-    else:
-        print("Error! Enter path!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='Path to file')
+    args = parser.parse_args()
+    filepath = args.filepath
+    get_most_frequent_words(data_prepare(load_data(filepath)))
